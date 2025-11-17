b8be12b44b1f1e6e94a230b99d13ebfc2b920af2
Gowri.Ch <gowri>
2025-11-17 23:45:57 +0530

    Home Valuation page updated

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+// Newly added pages
+import MapSearch from "./pages/MapSearch";
+import MarketTrends from "./pages/MarketTrends";
+import HomeValuation from "./pages/HomeValuation";
+import Agents from "./pages/Agents";
+import Blog from "./pages/Blog";
+import RecommendCommunities from "./pages/RecommendCommunities";
+import Contact from "./pages/Contact";
+
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+
+      <BrowserRouter>
+        <Routes>
+          {/* Home */}
+          <Route path="/" element={<Index />} />
+
+          {/* Navbar linked routes */}
+          <Route path="/map-search" element={<MapSearch />} />
+          <Route path="/market-trends" element={<MarketTrends />} />
+          <Route path="/home-valuation" element={<HomeValuation />} />
+          <Route path="/agents" element={<Agents />} />
+
+          {/* Tools dropdown */}
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/recommend-communities" element={<RecommendCommunities />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {/* Catch-all */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
